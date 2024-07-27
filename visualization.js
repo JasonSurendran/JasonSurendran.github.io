2419491bc5e4c613ce16823d0ab41a9bb0927f5e
Jason Surendran <surendranjason@gmail.com>
2024-07-27 09:43:22 -0500

    Blank

--- visualization.js
+++ visualization.js
@@ -1,6 +1,6 @@
 d3.csv('complete_renewable_energy_dataset.csv').then(data => {
     const energyTypes = Array.from(new Set(data.map(d => d['Energy Type'])));
-    const years = Array.from(new Set(data.map(d => d['Year']))).sort((a, b) => a - b);
+    const years = Array.from(new Set(data.map(d => +d['Year']))).sort((a, b) => a - b); // Ensure years are numbers
     const minYear = d3.min(years);
     const maxYear = d3.max(years);
 
